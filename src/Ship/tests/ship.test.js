96d3430cf28cb6ed6167cd3b42b5c85478dbf3a1
Hamna Ishaq <hamnaishaq1@gmail.com>
2023-05-23 23:33:18 +0500

    modify tests to check for hit and get hit counter ftns

--- src/Ship/tests/ship.test.js
+++ src/Ship/tests/ship.test.js
@@ -7,22 +7,33 @@ describe("Ship tests", () => {
   });
 
   test("Medium ship of length 4 cannot be hit 5 times", () => {
-    const ship = Ship(4, 5);
+    const ship = Ship(4);
+    expect(ship.hit()).toBe(1);
+    expect(ship.hit()).toBe(2);
+    expect(ship.hit()).toBe(3);
+    expect(ship.hit()).toBe(4);
     expect(ship.hit()).toBe("Ship cannot be hit more than its length");
   });
 
   test("Medium ship of length 4 is hit 2 times", () => {
-    const ship = Ship(4, 2);
+    const ship = Ship(4);
+    expect(ship.hit()).toBe(1);
     expect(ship.hit()).toBe(2);
   });
 
   test("Medium ship of length 4 is hit 2 times, it has not sunk", () => {
-    const ship = Ship(4, 2);
+    const ship = Ship(4);
+    expect(ship.hit()).toBe(1);
+    expect(ship.hit()).toBe(2);
     expect(ship.isSink()).toBeFalsy();
   });
 
   test("Medium ship of length 4 is hit 4 times, the ship has sunk!", () => {
-    const ship = Ship(4, 4);
+    const ship = Ship(4);
+    expect(ship.hit()).toBe(1);
+    expect(ship.hit()).toBe(2);
+    expect(ship.hit()).toBe(3);
+    expect(ship.hit()).toBe(4);
     expect(ship.isSink()).toBeTruthy();
   });
 });
