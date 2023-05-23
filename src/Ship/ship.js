77580a7a5ede01d7754e0ebb1605e8b6e359f7da
Hamna Ishaq <hamnaishaq1@gmail.com>
2023-05-23 23:31:50 +0500

    increment hitCount on triggering hit ftn and get current hit count with a getter function

--- src/Ship/ship.js
+++ src/Ship/ship.js
@@ -1,23 +1,29 @@
-const Ship = (length, hitCount = 0) => {
-  const len = () => {
+const Ship = (length) => {
+  let hitCount = 0;
+  function len() {
     return length;
-  };
+  }
 
-  const hit = () => {
+  function hit() {
     // by default hit is 0, and hit is maximun eq to length of ship
+    hitCount++;
     if (hitCount > len()) {
       return "Ship cannot be hit more than its length";
     }
     return hitCount;
-  };
+  }
 
-  const isSink = () => {
+  function getCurrentHitCount() {
+    return hitCount;
+  }
+
+  function isSink() {
     // get number of hits, get length of ship, compare the 2 if they are equal, ship has sunk, else not sunken
     const shipLength = len();
-    const hitCounter = hit();
+    const hitCounter = getCurrentHitCount();
 
     return hitCounter === shipLength ? true : false;
-  };
+  }
 
   return {
     len,
