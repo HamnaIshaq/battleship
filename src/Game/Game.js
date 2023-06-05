2f62fdc70278be765bec7b56d75e152329eba0fd
Hamna Ishaq <hamnaishaq1@gmail.com>
2023-06-06 00:21:37 +0500

    Save ship placements in an array

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -29,47 +29,63 @@ const Game = (player1Name, player2Name) => {
   }
 
   function player1Ships() {
+    const shipsArr = [];
+
     const player1PatrolBoat = Ship("patrol boat", 2);
     const player1StartingPosition = [0, 0];
     const player1DirectionOnBoard = "horizontal";
 
-    player1.board.placeShip(
-      player1PatrolBoat,
-      player1StartingPosition,
-      player1DirectionOnBoard
+    shipsArr.push(
+      player1.board.placeShip(
+        player1PatrolBoat,
+        player1StartingPosition,
+        player1DirectionOnBoard
+      )
     );
 
     const player1Submarine = Ship("submarine", 3);
     const player1StartingPositionSubmarine = [5, 0];
     const player1DirectionOnBoardSubmarine = "vertical";
 
-    player1.board.placeShip(
-      player1Submarine,
-      player1StartingPositionSubmarine,
-      player1DirectionOnBoardSubmarine
+    shipsArr.push(
+      player1.board.placeShip(
+        player1Submarine,
+        player1StartingPositionSubmarine,
+        player1DirectionOnBoardSubmarine
+      )
     );
+
+    return shipsArr;
   }
 
   function player2Ships() {
+    const shipsArr = [];
+
     const player2PatrolBoat = Ship("patrol boat", 2);
     const player2StartingPosition = [2, 0];
     const player2DirectionOnBoard = "horizontal";
 
-    player2.board.placeShip(
-      player2PatrolBoat,
-      player2StartingPosition,
-      player2DirectionOnBoard
+    shipsArr.push(
+      player2.board.placeShip(
+        player2PatrolBoat,
+        player2StartingPosition,
+        player2DirectionOnBoard
+      )
     );
 
     const player2Submarine = Ship("submarine", 3);
     const player2StartingPositionSubmarine = [5, 3];
     const player2DirectionOnBoardSubmarine = "vertical";
 
-    player2.board.placeShip(
-      player2Submarine,
-      player2StartingPositionSubmarine,
-      player2DirectionOnBoardSubmarine
+    shipsArr.push(
+      player2.board.placeShip(
+        player2Submarine,
+        player2StartingPositionSubmarine,
+        player2DirectionOnBoardSubmarine
+      )
     );
+
+    return shipsArr;
   }
 
   return {
